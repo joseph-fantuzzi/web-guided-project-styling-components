0e202389da8784a6a206e6a4c803ed17e64eea92
Casey Harding <casey.r.harding@gmail.com>
2022-01-20 12:42:18 -0500

    Through initial Friend component styling.

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+.friend {
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+} */
+
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+`
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ worker.start()
 
 import React from 'react'
 import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components';
 
 import App from './components/App'
 
 import './styles.css'
+import theme from './theme'
 
 render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.querySelector('#root')
 )
